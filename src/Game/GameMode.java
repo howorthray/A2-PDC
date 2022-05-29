05dd26e4b34cd17c091c063cb2a871afc83cd99c
howorthray <91508646+howorthray@users.noreply.github.com>
2022-05-29 18:29:11 +1200

    Game mode interface

---
+++ src/Game/GameMode.java
@@ -0,0 +1,19 @@
+package Game;
+
+/**
+ *
+ * @author xzy8653
+ */
+public abstract class GameMode {
+    
+    String name;
+    int cost;
+    int reward;
+    int numQuestions;
+    
+    public GameMode(){
+
+    }
+    
+
+}
